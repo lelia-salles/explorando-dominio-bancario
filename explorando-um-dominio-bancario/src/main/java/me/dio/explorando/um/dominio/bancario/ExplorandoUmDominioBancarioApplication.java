c2ddc0efce24ad3c5ae5a35597820e82953dce4f
Telecentro@CDI-05 <Telecentro@CDI-05>
2024-09-30 11:23:02 -0300

    create springboot project

---
+++ explorando-um-dominio-bancario/src/main/java/me/dio/explorando/um/dominio/bancario/ExplorandoUmDominioBancarioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.explorando.um.dominio.bancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorandoUmDominioBancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorandoUmDominioBancarioApplication.class, args);
+	}
+
+}
---
+++ explorando-um-dominio-bancario/src/test/java/me/dio/explorando/um/dominio/bancario/ExplorandoUmDominioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.explorando.um.dominio.bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorandoUmDominioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
