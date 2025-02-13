d1a525bd76359ad0c1d4fcc89336d965e9a4d193
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-02-13 15:31:01 +0000

    configuração de variável de ambiente

--- dominioBancario/src/main/java/me/dio/domain/repository/UserRepository.java
+++ dominioBancario/src/main/java/me/dio/domain/repository/UserRepository.java
@@ -4,7 +4,7 @@ import me.dio.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // não há necessidade dessa anotação,mas para ficar mais claro é bom colocar
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByAccountNumber(String accountNumber);
