f841d377d37a4c5f51916e7e2173d44980eedeb2
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-02-14 09:31:08 -0300

    update

--- dominioBancario/src/main/java/me/dio/service/impl/UserServiceImpl.java
+++ dominioBancario/src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -16,7 +16,7 @@ Essa implementação atua como intermediária entre a camada de apresentação (
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    private final UserRepository userRepository; // injeção de dependência: userRepository é passado para a classe via seu construtor, em vez de ser criado diretamente dentro dela. Isso facilita a testabilidade, promove a reutilização de código e separa as responsabilidades, seguindo boas práticas de design.
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -24,13 +24,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User findById(Long id) {
-        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new); // O método findById busca um usuário pelo ID usando o repositório. Se o usuário não for encontrado, lança uma exceção NoSuchElementException, garantindo que valores nulos não sejam retornados.
     }
 
     @Override
     public User create(User userToCreate) {
         if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
-            throw new IllegalArgumentException("This Account number already exists.");
+            throw new IllegalArgumentException("This Account number already exists."); // O método create cria um novo usuário. Ele verifica se já existe uma conta com o mesmo número usando existsByAccountNumber. Se existir, lança uma exceção IllegalArgumentException. Caso contrário, salva o usuário no banco com o método save.
         }
         return userRepository.save(userToCreate);
     }
