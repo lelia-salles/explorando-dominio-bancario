64f901908fbdbc0c0ffec671e2b112106c9d5e98
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-02-14 09:20:57 -0300

    update

--- dominioBancario/src/main/java/me/dio/service/UserService.java
+++ dominioBancario/src/main/java/me/dio/service/UserService.java
@@ -4,7 +4,11 @@ import me.dio.domain.model.User;
 
 public interface UserService {
 
-    User findById(Long id);
+// a implementação está em UserServiceImpl    
+ 
+public interface UserService {
 
-    User create(User userToCreate);
+    User findById(Long id);// retorna o usuário pelo id
+
+    User create(User userToCreate);//cria o usuário
 }
