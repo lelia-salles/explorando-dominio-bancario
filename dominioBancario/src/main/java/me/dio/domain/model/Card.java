dae15507d95ea36be1587cc8f68a3a4aecda5759
Lelia Salles <134879917+lelia-salles@users.noreply.github.com>
2025-02-14 08:42:18 -0300

    Update Card.java

--- dominioBancario/src/main/java/me/dio/domain/model/Card.java
+++ dominioBancario/src/main/java/me/dio/domain/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(unique = true)
     private String number;
 
-    @Column(name = "available_limit", precision = 13, scale = 2)
+    @Column(name = "available_limit", precision = 2, scale = 13)
     private BigDecimal limit;
 
     public Long getId() {
