87a0a175ccb9dbdb6fb8651426d3b4de30ad482a
unknown <leliasallesh@gmail.com>
2025-04-27 21:54:14 -0300

    fix build gradle

--- dominioBancario/src/main/java/me/dio/Application.java
+++ dominioBancario/src/main/java/me/dio/Application.java
@@ -1,14 +1,16 @@
 package me.dio.explorando.um.dominio.bancario;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(server = {@Server(url = "/", description = "Default Server URL")}) //para contexto do Swagger e corrigir problema de CORS gerados por conflitos https (Railway) e http (OpenAPI Swagger)
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExplorandoUmDominioBancarioApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
